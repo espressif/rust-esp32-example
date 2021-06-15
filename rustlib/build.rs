a0641abd8c42475e56f70d2ee3ddcdb91fcbfe97
Robert Cottrell <rgcottrell@gmail.com>
2021-06-15 10:48:12 +0100

    Enable bindgen during cargo build (#9)
    * Add "std" feature to rustlib to allow using std in Xtensa projects
    * Fix cargo target in CMakeLists.txt
    * Use std feature when building for Xtensa
    * Use std ffi aliases when building with std feature
    * Add custom ffi aliases when building without std feature
    * Add no_std attribute and panic handler when building without std feature

--- rustlib/build.rs
+++ rustlib/build.rs
@@ -1,40 +1,32 @@
 use std::env;
-use std::fs;
 use std::path::{Path, PathBuf};
 
 fn main() {
     let target = env::var("TARGET").unwrap();
 
-    // let cargo_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-
+    let cargo_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    // fs::remove_dir_all(&out_dir).unwrap();
-    // fs::create_dir_all(&out_dir).unwrap();
 
-    // run_cbindgen(&cargo_dir, &out_dir);
+    run_cbindgen(&cargo_dir, &out_dir);
     run_bindgen(&target, &out_dir);
 }
 
-// fn run_cbindgen(_cargo_dir: &Path, _out_dir: &Path) {
+fn run_cbindgen(_cargo_dir: &Path, _out_dir: &Path) {
     // TODO: Run cbindgen
-// }
+}
 
 fn run_bindgen(target: &str, out_dir: &Path) {
-    // if target == "xtensa-esp32-none-elf" {
-    //     env::set_var("LLVM_CONFIG_PATH", "/usr/local/xtensa/llvm/bin/llvm-config");
-    // }
-
     let mut builder = bindgen::Builder::default();
     builder = builder.header("../main/CApi.h");
-    builder = builder.use_core();
-    builder = builder.ctypes_prefix("ffi");
-
-    println!("Target: {}", target);
     match target {
         "riscv32i-unknown-none-elf" => {
             builder = builder.clang_arg("--target=riscv32");
+            builder = builder.use_core();
+            builder = builder.ctypes_prefix("crate::ffi");
         }
         "xtensa-esp32-none-elf" => {
+            // Make sure that LLVM_CONFIG_PATH has been set to point to the
+            // Xtensa build of llvm-config.
             builder = builder.clang_arg("--target=xtensa-esp32-elf");
         }
         _ => {
